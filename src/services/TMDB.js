5848a97ccb7ea10f8d296d25ecdf7390c31837e7
Imkamalesh <adamkamalesh@gmail.com>
2023-08-22 00:12:41 +0530

    authentication using imdb

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 const tmdbApiKey = process.env.REACT_APP_TMDB_KEY ;
-const page = 1;
 // https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
 
 export const tmdbApi = createApi({
@@ -14,7 +13,31 @@ export const tmdbApi = createApi({
         }),
         //* Get Movie by [Type]
         getMovies: builder.query({
-            query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
+            query: ({ genreIdOrCategoryName, page, searchQuery }) => {
+                // Get Movies by Search
+                if (searchQuery) {
+                    return `/search/movie?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}`;
+                }
+
+                //* Get Movies by Category
+                if (
+                    genreIdOrCategoryName &&
+                    typeof genreIdOrCategoryName === "string"
+                ) {
+                    return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`;
+                }
+
+                //* Get Movies by Genre
+                if (
+                    genreIdOrCategoryName &&
+                    typeof genreIdOrCategoryName === "number"
+                ) {
+                    return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`;
+                }
+
+                 // Get popular Movies
+                 return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
+            },
         }),
     }),
 });
